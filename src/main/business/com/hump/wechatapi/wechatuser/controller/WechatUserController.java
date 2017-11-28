d4db51d92d1da99f8242b25532cb6cf5a398df65
zackma <chattmazio@gmail.com>
2017-11-28 13:51:02 +0800

    新增微信用户业务代码和微信用户接口demo

--- src/main/business/com/hump/wechatapi/wechatuser/controller/WechatUserController.java
+++ src/main/business/com/hump/wechatapi/wechatuser/controller/WechatUserController.java
@@ -1,7 +1,9 @@
 package com.hump.wechatapi.wechatuser.controller;
 
 import com.hump.wechatuser.entity.TWechatFansEntity;
+import com.hump.wechatuser.service.TWechatFansServiceI;
 import org.jeecgframework.core.common.model.json.AjaxJson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,26 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping(value = "/wechatUserApi")
 public class WechatUserController {
 
+    @Autowired
+    private TWechatFansServiceI wechatFansService;
+
     @RequestMapping(value = "/insertUsers")
     @ResponseBody
     public AjaxJson insertUsers(HttpServletRequest request, HttpServletResponse response, TWechatFansEntity wechatFans){
         AjaxJson j = new AjaxJson();
         String msg = "新增微信用户成功！";
-        //do something...
+        String openid = wechatFans.getOpenid()==null?"":wechatFans.getOpenid();
+        if(!"".equals(openid)){
+            try {
+                wechatFansService.save(wechatFans);
+            }catch (Exception e){
+                msg = "新增业务执行失败！";
+                e.printStackTrace();
+            }
+        }else{
+            msg = "openid为空，新增微信用户失败！";
+            j.setObj(false);
+        }
         j.setMsg(msg);
         return j;
     }
